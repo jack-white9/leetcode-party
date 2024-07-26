a41ab3a81079b63ca23e700fbe646ee9a0b37b30
Jack White <jfw793@gmail.com>
2024-07-26 21:58:11 +1000

    save friends to localStorage to persist data across sessions

--- src/App.jsx
+++ src/App.jsx
@@ -3,51 +3,46 @@ import { useEffect, useState } from "react";
 import { Box, Skeleton, Stack, Heading, Input, Flex } from "@chakra-ui/react";
 import "./App.css";
 
-const getUserData = async (user) => {
-  const url = "https://leetcode-stats-api.herokuapp.com/" + user;
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-    let json = await response.json();
-    json = { ...json, id: user };
-    return json;
-  } catch (error) {
-    console.error(error.message);
-    return null;
-  }
-};
-
 export const App = () => {
-  const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [users, setUsers] = useState(
+    JSON.parse(localStorage.getItem("users")) || []
+  );
+  const [isLoading, setIsLoading] = useState(false);
   const [value, setValue] = useState("");
+
+  console.log(users);
+
   const handleChange = (event) => setValue(event.target.value);
+
   const handleKeyDown = async (event) => {
     if (event.key === "Enter") {
       const newUser = await getUserData(value);
       if (newUser) {
         setUsers((prevUsers) => [newUser, ...prevUsers]);
-        console.log(users); // Note: This might not log the updated state immediately
       }
+      setValue("");
+    }
+  };
+
+  const getUserData = async (user) => {
+    const url = "https://leetcode-stats-api.herokuapp.com/" + user;
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      let json = await response.json();
+      json = { ...json, id: user };
+      return json;
+    } catch (error) {
+      console.error(error.message);
+      return null;
     }
   };
 
   useEffect(() => {
-    const fetchUserData = async () => {
-      const inputted_user = "jekwat";
-      const user = await getUserData(inputted_user);
-      if (user) {
-        setUsers([user]);
-      } else {
-        console.error("Failed to fetch user data");
-      }
-      setIsLoading(false);
-    };
-
-    fetchUserData();
-  }, []);
+    localStorage.setItem("users", JSON.stringify(users));
+  }, [users]);
 
   return (
     <Flex
